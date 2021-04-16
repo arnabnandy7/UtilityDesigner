5de66ff57f8951b3a62a49c265d2ed1f4562dd12
Arnab Nandy <arnab_nandy7@yahoo.com>
2021-04-16 23:37:19 +0530

    Creating Utility design project

---
+++ src/main/java/com/arnab/utility/UtilityDesignerApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityDesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityDesignerApplication.class, args);
+	}
+
+}
